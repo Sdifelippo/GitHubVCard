7384582d0b6a185e9215c0f40de9ca2c123bd909
Steven Difelippo <matrixdriver04@gmail.com>
2017-07-17 18:43:07 -0400

    update2

--- GitHubData.js
+++ GitHubData.js
@@ -1,15 +1,34 @@
-let name = document.querySelector('.name');
-let basic = document.querySelector('basics');
-let story = document.querySelector('story');
+let header = document.querySelector(".header");
+let basics = document.querySelector(".basics");
+let story = document.querySelector(".story");
+let picture = document.querySelector(".picture");
+
 
 let req = new XMLHttpRequest();
 
-req.open("GET", "https://api.github.com/users/sdifelippo");
-req.addEventListener("load", function(){
-    let obj = JSON.parse(req.response);
-     name.innerHTML += `
+req.open("GET", "https://api.github.com/users/Sdifelippo");
+req.addEventListener("load", function() {
+  let obj = JSON.parse(req.response);
 
-     <h1>${obj.name}</h1>`
-    //  <p> </p>
+  header.innerHTML += `
+  <h1>${obj.name}</h1>
+  `
+
+  basics.innerHTML += `
+     <h2>The Basics<h2>
+     <li><span>Name: </span>${obj.name}</li>
+     <li><span>GitHub URL: </span> <a href="${obj.url}">Sdifelippo<a></li>
+     <li><span>Location: </span>${obj.location}</li>
+     <li><span>Company: </span>${obj.company}</li>
+     <li><span>Website: </span><a href="${obj.blog}">MyAwesomeWebsite.com</a></li>
+     `
+  story.innerHTML += `
+     <h2>The Story:</h2> <a> ${obj.bio} </a>
+
+     `
+   picture.innerHTML += `
+   <img src="${obj.avatar_url}">
+   `
 });
-req.send();
+
+req.send()
